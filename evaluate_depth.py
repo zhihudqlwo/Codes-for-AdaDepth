d9b2479aa20a86f58289c13f1d2e53682a2e9109
zhihudqlwo <87171094+zhihudqlwo@users.noreply.github.com>
2025-09-21 13:50:36 +0800

    Update evaluate_depth.py

--- evaluate_depth.py
+++ evaluate_depth.py
@@ -12,6 +12,27 @@ import networks
 import numpy as np
 from layers import *
 import pdb
+import os
+import requests
+from tqdm import tqdm
+
+def download_file(url, save_path):
+    """下载文件并带进度条"""
+    if os.path.exists(save_path):
+        print(f"✔ Found {save_path}, skip downloading")
+        return save_path
+    
+    print(f"⬇ Downloading {url} to {save_path}")
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    response = requests.get(url, stream=True)
+    total = int(response.headers.get("content-length", 0))
+    with open(save_path, "wb") as file, tqdm(
+        desc=save_path, total=total, unit="B", unit_scale=True, unit_divisor=1024
+    ) as bar:
+        for data in response.iter_content(chunk_size=1024):
+            size = file.write(data)
+            bar.update(size)
+    return save_path
 
 warnings.filterwarnings("ignore")
 
@@ -134,6 +155,14 @@ def load_model(args):
 
     encoder_path = os.path.join(args.pretrained_path, "encoder_ref.pth")
     decoder_path = os.path.join(args.pretrained_path, "depth_ref.pth")
+
+    # 如果本地不存在
+    encoder_url = "https://github.com/zhihudqlwo/Codes-for-AdaDepth/releases/download/v1.0/encoder_ref.pth"
+    decoder_url = "https://github.com/zhihudqlwo/Codes-for-AdaDepth/releases/download/v1.0/depth_ref.pth"
+
+    encoder_path = download_file(encoder_url, encoder_path)
+    decoder_path = download_file(decoder_url, decoder_path)
+
 
     # encoder_path = os.path.join(args.pretrained_path, "encoder.pth")
     # decoder_path = os.path.join(args.pretrained_path, "depth.pth") # for coarse depth
